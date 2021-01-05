3297055bee99d77385b8396813ec2372badce799
Luis Sanchez <luisfsanchez215@gmail.com>
2021-01-05 14:04:49 -0500

    Shouold work better

---
+++ arm/simple_controller_optimized.py
@@ -0,0 +1,52 @@
+
+# Import libraries
+import pigpio
+import time
+
+pigpio.start()
+
+try:
+    while True:
+        data = float(str(input('Enter angle between 0 & 180: ')))
+        pigpio.set_servo_pulsewidth(18, data)
+        # data2 = data.split(', ')
+        # if data2[0] == "A":
+        #     print("First motor turning\n")
+        #     angle = float(data2[1])
+        #     if (angle<0) or (angle>180):
+        #         print("Wrong angle value inputed \n")  
+        #     else:
+        #         servoA.ChangeDutyCycle(2+(angle/18))
+        #         print("Angle value changed to " + data2[1] + "\n") 
+
+        # elif data2[0] == "B":
+        #     print("Second motor turning\n")
+        #     angle = float(data2[1])
+        #     if (angle<0) or (angle>180):
+        #         print("Wrong angle value inputed \n")  
+        #     else:
+        #         servoB.ChangeDutyCycle(2+(angle/18))
+        #         print("Angle value changed to " + data2[1] + "\n") 
+        # elif data2[0] == "C":
+        #     print("Third motor turning\n")
+        #     angle = float(data2[1])
+        #     if (angle<0) or (angle>180):
+        #         print("Wrong angle value inputed \n")  
+        #     else:
+        #         servoC.ChangeDutyCycle(2+(angle/18))
+        #         print("Angle value changed to " + data2[1] + "\n") 
+        # elif data2[0] == "D":
+        #     print("Fourth motor turning\n")
+        #     angle = float(data2[1])
+        #     if (angle<150) or (angle>180):
+        #         print("Wrong angle value inputed \n")  
+        #     else:
+        #         servoA.ChangeDutyCycle(2+(angle/18))
+        #         print("Angle value changed to " + data2[1] + "\n") 
+        # else:
+        #     print("Not recognized this input : " + data2[0]+"\n")
+
+finally:
+    # Clean things up at the end
+    pigpio.stop()
+    print("Done !")
