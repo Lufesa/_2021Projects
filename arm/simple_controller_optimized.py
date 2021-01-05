194566675fbd481aebac56a70264d4dd2bf2869f
Luis Sanchez <luisfsanchez215@gmail.com>
2021-01-05 15:07:21 -0500

    Added all the controls

--- arm/simple_controller_optimized.py
+++ arm/simple_controller_optimized.py
@@ -12,43 +12,42 @@ pi = pigpio.pi()
 try:
     while True:
         data = float(str(input('Enter angle between 0 & 180: ')))
-        pi.set_servo_pulsewidth(22, data)
-        # data2 = data.split(', ')
-        # if data2[0] == "A":
-        #     print("First motor turning\n")
-        #     angle = float(data2[1])
-        #     if (angle<0) or (angle>180):
-        #         print("Wrong angle value inputed \n")  
-        #     else:
-        #         servoA.ChangeDutyCycle(2+(angle/18))
-        #         print("Angle value changed to " + data2[1] + "\n") 
+        data2 = data.split(', ')
+        if data2[0] == "A":
+            print("First motor turning\n")
+            angle = float(data2[1])
+            if (angle<0) or (angle>180):
+                print("Wrong angle value inputed \n")  
+            else:
+                pi.set_servo_pulsewidth(17, angle*(2460-500)/180.0 + 500)
+                print("Angle value changed to " + data2[1] + "\n") 
 
-        # elif data2[0] == "B":
-        #     print("Second motor turning\n")
-        #     angle = float(data2[1])
-        #     if (angle<0) or (angle>180):
-        #         print("Wrong angle value inputed \n")  
-        #     else:
-        #         servoB.ChangeDutyCycle(2+(angle/18))
-        #         print("Angle value changed to " + data2[1] + "\n") 
-        # elif data2[0] == "C":
-        #     print("Third motor turning\n")
-        #     angle = float(data2[1])
-        #     if (angle<0) or (angle>180):
-        #         print("Wrong angle value inputed \n")  
-        #     else:
-        #         servoC.ChangeDutyCycle(2+(angle/18))
-        #         print("Angle value changed to " + data2[1] + "\n") 
-        # elif data2[0] == "D":
-        #     print("Fourth motor turning\n")
-        #     angle = float(data2[1])
-        #     if (angle<150) or (angle>180):
-        #         print("Wrong angle value inputed \n")  
-        #     else:
-        #         servoA.ChangeDutyCycle(2+(angle/18))
-        #         print("Angle value changed to " + data2[1] + "\n") 
-        # else:
-        #     print("Not recognized this input : " + data2[0]+"\n")
+        elif data2[0] == "B":
+            print("Second motor turning\n")
+            angle = float(data2[1])
+            if (angle<0) or (angle>180):
+                print("Wrong angle value inputed \n")  
+            else:
+                pi.set_servo_pulsewidth(18, angle*(2430-510)/180.0 + 510)
+                print("Angle value changed to " + data2[1] + "\n") 
+        elif data2[0] == "C":
+            print("Third motor turning\n")
+            angle = float(data2[1])
+            if (angle<0) or (angle>180):
+                print("Wrong angle value inputed \n")  
+            else:
+                pi.set_servo_pulsewidth(27, angle*(2500-500)/180.0 + 500)
+                print("Angle value changed to " + data2[1] + "\n") 
+        elif data2[0] == "D":
+            print("Fourth motor turning\n")
+            angle = float(data2[1])
+            if (angle<150) or (angle>180):
+                print("Wrong angle value inputed \n")  
+            else:
+                pi.set_servo_pulsewidth(22, angle*(2500-1500)/180.0 + 1500)
+                print("Angle value changed to " + data2[1] + "\n") 
+        else:
+            print("Not recognized this input : " + data2[0]+"\n")
 
 finally:
     # Clean things up at the end
